1035d83748f73e610fc040011fbe77a513cc9587
Sirathee Koomgreng <ska514@sfu.ca>
2026-03-26 13:07:50 -0700

    use workflow template instead

--- infrastructure/dataproc.tf
+++ infrastructure/dataproc.tf
@@ -1,7 +1,11 @@
 # dataproc.tf
+locals {
+  cluster_name  = "${local.name_prefix}-cluster"
+  template_name = "${local.name_prefix}-train-regression-template"
+}
 resource "google_dataproc_autoscaling_policy" "og_gdelt_autoscaling" {
   policy_id = "${local.name_prefix}-autoscaling-policy"
-  location    = var.gcp_region
+  location  = var.gcp_region
 
   worker_config {
     min_instances = 2 # minimum 2 total nodes
@@ -17,40 +21,51 @@ resource "google_dataproc_autoscaling_policy" "og_gdelt_autoscaling" {
   }
 }
 
-resource "google_dataproc_cluster" "og_gdelt_cluster" {
-  name   = "${local.name_prefix}-cluster"
-  region = var.gcp_region
 
-  cluster_config {
-    # Uses the bucket created in gcs.tf for temporary staging
-    staging_bucket = google_storage_bucket.main_data.name
-    temp_bucket    = google_storage_bucket.main_data.name
+resource "google_dataproc_workflow_template" "template" {
+  name     = local.template_name
+  location = var.gcp_region
+  placement {
+    managed_cluster {
+      cluster_name = local.cluster_name
+      config {
+        # Uses the bucket created in gcs.tf for temporary staging
+        staging_bucket = google_storage_bucket.main_data.name
+        temp_bucket    = google_storage_bucket.main_data.name
 
-    master_config {
-      num_instances = 1 # initial number of master nodes
-      machine_type  = "e2-standard-4"
-      disk_config {
-        # boot_disk_type    = "pd-ssd"
-        boot_disk_size_gb = 30 # Keeps storage costs minimal (GBs)
+        master_config {
+          num_instances = 1 # initial number of master nodes
+          machine_type  = "e2-standard-4"
+          disk_config {
+            boot_disk_size_gb = 30 # Keeps storage costs minimal (GBs)
+          }
+        }
+
+        worker_config {
+          num_instances = 2 # Initial number of worker nodes
+          machine_type  = "e2-standard-4"
+          disk_config {
+            boot_disk_size_gb = 30
+          }
+        }
+
+        # Links to the custom network created in network.tf
+        gce_cluster_config {
+          subnetwork = google_compute_subnetwork.custom_subnet.id
+        }
+
+        autoscaling_config {
+          policy = google_dataproc_autoscaling_policy.og_gdelt_autoscaling.name
+        }
       }
-    }
 
-    worker_config {
-      num_instances = 2 # Initial number of worker nodes
-      machine_type  = "e2-standard-4"
-      disk_config {
-        # boot_disk_type    = "pd-ssd"
-        boot_disk_size_gb = 30
-      }
     }
-
-    # Links to the custom network created in network.tf
-    gce_cluster_config {
-      subnetwork = google_compute_subnetwork.custom_subnet.id
+  }
+  jobs {
+    step_id = "train-regression-models"
+    pyspark_job {
+      main_python_file_uri = "gs://${google_storage_bucket.main_data.name}/scripts/pys"
     }
-
-    autoscaling_config {
-      policy_uri = google_dataproc_autoscaling_policy.og_gdelt_autoscaling.name
-    }
+    
   }
 }
