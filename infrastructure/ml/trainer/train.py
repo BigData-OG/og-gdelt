bbade90f51b7a2eac255c115ece1117b74e0341c
Sirathee Koomgreng <ska514@sfu.ca>
2026-03-28 21:20:09 -0700

    fix packaging

--- infrastructure/ml/trainer/train.py
+++ infrastructure/ml/trainer/train.py
@@ -7,7 +7,6 @@ import joblib
 
 def load_config():
     config_path = os.path.join(os.path.dirname(__file__), 'config.json')
-    
     with open(config_path, 'r') as f:
         config = json.load(f)
     return config
