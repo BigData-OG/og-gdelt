c11ee48a83c49d96b3cb2ee541aa48f60eaaeea3
Sirathee Koomgreng <ska514@sfu.ca>
2026-03-28 21:38:55 -0700

    fix tf state

--- infrastructure/vertex-ai.tf
+++ infrastructure/vertex-ai.tf
@@ -10,7 +10,7 @@ resource "local_file" "dynamic_ml_config" {
 resource "null_resource" "packaged_ml_code" {
   depends_on = [local_file.dynamic_ml_config]
   triggers = {
-    file_hash = filesha256("${path.module}/ml/trainer/train.py")
+    key = filemd5("${path.module}/ml/trainer/train.py") # Re-run if the training script changes
   }
   provisioner "local-exec" {
     command = "python ${path.module}/ml/setup.py sdist --formats=gztar"
@@ -23,4 +23,7 @@ resource "google_storage_bucket_object" "vertex_ai_model_training_package" {
   name   = "scripts/training_package.tar.gz"
   source = "${path.module}/dist/gdelt_trainer-0.1.tar.gz"
   bucket = google_storage_bucket.main_data.name
+  lifecycle {
+    replace_triggered_by = [ null_resource.packaged_ml_code ]
+  }
 }
