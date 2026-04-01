41dd1fed6a748fe7bc525329e3ce29009eb45ae2
Sirathee Koomgreng <ska514@sfu.ca>
2026-03-31 22:36:25 -0700

    fix stuff

--- api/services/model_repository.py
+++ api/services/model_repository.py
@@ -1,9 +1,16 @@
+import os
 from google.cloud import firestore
 
 class ModelRepository:
-    def __init__(self, firestore_client: firestore.Client):
-        self.firestore_client = firestore_client
-        self.collection_name = "gdeltModels"
+    _instance = None
+
+    def __new__(cls, firestore_client: firestore.Client = None):
+        if cls._instance is None:
+            cls._instance = super(ModelRepository, cls).__new__(cls)
+            # Initialize strictly once
+            cls._instance.firestore_client = firestore_client
+            cls._instance.collection_name = os.environ.get("FIRESTORE_COLLECTION_NAME", "gdeltModels")
+        return cls._instance
 
     def save_model_id(self, company_name, model_id):
         doc_ref = self.firestore_client.collection(self.collection_name).document(company_name)
